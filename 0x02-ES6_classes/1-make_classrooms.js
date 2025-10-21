6d43c80a2be8e95660ec9b94b551de424901791e
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2025-10-21 11:36:02 +0100

    1-make_classrooms.js create objects based on a class definition

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from "./0-classroom.js";
+
+export default function initializeRooms() {
+  const one = new ClassRoom(19);
+  const two = new ClassRoom(20);
+  const three = new ClassRoom(34);
+  const array = [one, two, three];
+  return array;
+}
