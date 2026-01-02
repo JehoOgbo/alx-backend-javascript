53fd6939ba6a50f61faf590e7550ba362a36b7b3
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2026-01-02 09:51:18 +0100

    Add regex type validation and test suite

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+
+const app = express();
+const port = 7865;
+
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (request, response) => {
+  const id = request.params.id;
+
+  response.send(`Payment methods for cart ${id}`);
+});
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
